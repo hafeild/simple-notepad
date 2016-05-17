24c88f2854f46b7fa514fa526c16b7028903501e
Henry Feild <hafeild@gmail.com>
2016-05-17 10:21:11 -0400

    Added error handling to the build script.

--- build.sh
+++ build.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+
 ## Build.
 echo -n "Building..."
 mkdir -p bin
 javac src/*.java -d bin
-jar cfe simple-notepad.jar SimpleNotepad -C bin .
-chmod +x simple-notepad.jar
-echo -e "done!\n"
+status=$?
+if [ $status -eq 0 ]; then
+    jar cfe simple-notepad.jar SimpleNotepad -C bin .
+    status=$?
+fi
 
-## Let the user know how to use it.
-echo -e "You can run the program either of the following methods:\n"
-echo -e "1.\t./simple-notepad"
-echo -e "2.\tjava -jar simple-notepad.jar"
-echo -e "3.\tdouble click simple-notepad.jar in a file browser\n"
+if [ $status -eq 0 ]; then
+    chmod +x simple-notepad.jar
+    echo -e "done!\n"
+
+    ## Let the user know how to use it.
+    echo -e "You can run the program either of the following methods:\n"
+    echo -e "1.\t./simple-notepad"
+    echo -e "2.\tjava -jar simple-notepad.jar"
+    echo -e "3.\tdouble click simple-notepad.jar in a file browser\n"
+else
+    echo -e "\nBuild failed.\n"
+fi
